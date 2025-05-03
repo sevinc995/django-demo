8520723e3675b3e8eff3c9432edebd3110b3a89c
sevinc995 <sevincseferova25@gmail.com>
2025-05-03 04:21:19 +0000

    changed

--- core/settings.py
+++ core/settings.py
@@ -31,9 +31,12 @@ INSTALLED_APPS = [
     # "django.contrib.staticfiles",
     "rest_framework",
     "api",
+    "corsheaders",
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -41,6 +44,10 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "https://sevinc995.github.io",
 ]
 
 ROOT_URLCONF = "core.urls"
